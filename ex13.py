a95094d28ce28c59fd8c806e33214d6055fa17a0
zzaibis <zzaibis@gmail.com>
2023-02-06 10:02:15 +0530

    initial commit

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print(f'The script is called {script}')
+print(f'The first variable is {first}')
+print(f'The second variable is {second}')
+print(f'The third variable is {third}')
